9536f195baf9adef4635a7d1265e3f10dae59985
EAGLE <87280284+eagle-s-learner@users.noreply.github.com>
2021-10-10 15:14:51 +0530

    Create calculator_in_c_language.c
    I'm new at code :)
    I created a calculator in C language. Please accept it and also add **hacktoberfest** label.
    Thank you :)

---
+++ calculator_in_c_language.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+  char op;
+  int a, b;
+  printf("Choose an operator from following "+, -, *, /": ");
+  scanf("%c", &op);
+  printf("\n");
+  printf("Enter two operands: \n");
+  scanf("%ld %ld", &a, &b);
+
+  switch (op) {
+    case '+':
+      printf("%ld + %ld = %dl", a, b, a + b);
+      break;
+    case '-':
+      printf("%la - %lb = %ld", a, b, a - b);
+      break;
+    case '*':
+      printf("%ld * %ld = %ld", a, b, a * b);
+      break;
+    case '/':
+      printf("%ld / %ld = %ld", a, b, a / b);
+      break;
+    // if choosen invalid operator
+    default:
+      printf("Error! Please choose valid operator");
+  }
+
+  return 0;
+}
